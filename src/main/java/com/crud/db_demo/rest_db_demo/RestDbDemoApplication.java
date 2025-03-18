c5f7fc6ef40358da86ddbdc6bfc219a59bdca419
klpdotnet <klpdotnet@gmail.com>
2025-03-18 10:32:35 -0500

    Initial checking for SQL CRUD operations

---
+++ src/main/java/com/crud/db_demo/rest_db_demo/RestDbDemoApplication.java
@@ -0,0 +1,13 @@
+package com.crud.db_demo.rest_db_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDbDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDbDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crud/db_demo/rest_db_demo/RestDbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.db_demo.rest_db_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestDbDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
